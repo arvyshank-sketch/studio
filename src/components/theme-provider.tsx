3f431621fe5680fe44a88da4f399dc1c6de4b3bd
ArvY <arvyshank@gmail.com>
2025-08-13 05:13:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,17 +1,45 @@
 'use client';
 
 import * as React from 'react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import {
+  ThemeProvider as NextThemesProvider,
+  useTheme as useNextTheme,
+} from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
 
+type CustomThemeContextProps = {
+  theme?: string;
+  setTheme: (theme: string) => void;
+};
+
 export const ThemeProviderContext = React.createContext<
-  | {
-      theme: string;
-      setTheme: (theme: string) => void;
-    }
-  | undefined
+  CustomThemeContextProps | undefined
 >(undefined);
 
+// This is a wrapper component that will have access to the theme from next-themes
+function ThemeProviderWrapper({ children }: { children: React.ReactNode }) {
+  const { theme, setTheme } = useNextTheme();
+
+  const value = React.useMemo(
+    () => ({
+      theme,
+      setTheme,
+    }),
+    [theme, setTheme]
+  );
+
+  return (
+    <ThemeProviderContext.Provider value={value}>
+      {children}
+    </ThemeProviderContext.Provider>
+  );
+}
+
+// This is the main provider that sets up next-themes
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider {...props}>
+      <ThemeProviderWrapper>{children}</ThemeProviderWrapper>
+    </NextThemesProvider>
+  );
 }
