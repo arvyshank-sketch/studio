19f5209655b0b3f51e0735f24ffa3457b948c678
ArvY <arvyshank@gmail.com>
2025-08-13 15:17:02 +0000

    Is this coding language can't load images in app?

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -40,7 +40,7 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
 
       if (!userDocSnap.exists()) {
         const newDisplayName = user.displayName || generateUsername(user.email!);
-        const photoURL = user.photoURL || `https://placehold.co/128x128.png`;
+        const photoURL = user.photoURL || `https://placehold.co/128x128/000000/FFFFFF.png`;
         try {
           // We only update the profile if it's missing a display name or photo
           if (!user.displayName || !user.photoURL) {
